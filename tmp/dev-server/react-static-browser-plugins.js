e7ba919b8d55e1dd6ae44ae15fc2dedaa9c71731
Victor Quiroz <victorhqc@gmail.com>
2019-01-13 18:39:44 +0100

    Default react-static

---
+++ tmp/dev-server/react-static-browser-plugins.js
@@ -0,0 +1,12 @@
+// Imports
+
+
+// Plugins
+const plugins = [{
+  location: "/Users/victor.castro/Documents/Code/salon-bugambilias-static",
+  plugins: [],
+  hooks: {}
+}]
+
+// Export em!
+export default plugins
