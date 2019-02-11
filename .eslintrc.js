7e8c79eef5fa3f6675148c133083e4f7d99331e1
Victor Quiroz Castro <victorhqc@gmail.com>
2019-02-11 22:25:47 +0100

    Simplify eslint & update prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,7 @@
 module.exports = {
-    extends: 'airbnb',
-    rules: {
-        'react/jsx-filename-extension': 0,
-        'react/no-unescaped-entities': 0,
-        'import/prefer-default-export': 0,
-    },
-    env: {
-        browser: true,
-        node: true,
-    },
+  extends: ['healthier', 'healthier/react'],
+  env: {
+    browser: true,
+    node: true,
+  },
 };
