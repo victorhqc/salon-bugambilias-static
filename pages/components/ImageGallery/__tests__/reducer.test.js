d42b59aa0779f40c098ea4a1358ebbd995b22d2a
Victor Quiroz Castro <victorhqc@gmail.com>
2019-03-31 20:13:32 +0200

    Prepare reducer for gallery Half of the code is from fader. Could be reused.

---
+++ pages/components/ImageGallery/__tests__/reducer.test.js
@@ -0,0 +1,112 @@
+import {
+  PREVIOUS_IMAGE,
+  NEXT_IMAGE,
+  imageGalleryReducer,
+  nextImage,
+  previousImage,
+  getDefaultState,
+} from '../reducer';
+
+const dog = { src: '🐶', alt: 'Dog' };
+const cat = { src: '🐱', alt: 'Cat' };
+const hamster = { src: '🐹', alt: 'Hamster' };
+const panda = { src: '🐼', alt: 'Panda' };
+const tiger = { src: '🐯', alt: 'Tiger' };
+
+describe('visibleImages actions', () => {
+  it('Should render next image', () => {
+    expect(nextImage()).toEqual({ type: NEXT_IMAGE });
+  });
+
+  it('Shouls render previous image', () => {
+    expect(previousImage()).toEqual({ type: PREVIOUS_IMAGE });
+  });
+});
+
+describe('imageGalleryReducer', () => {
+  const defaultInitialState = {
+    images: [dog, cat, hamster, panda, tiger],
+  };
+
+  it('Should return default state', () => {
+    expect(imageGalleryReducer()).toEqual({
+      images: [],
+    });
+  });
+
+  it('Should return initial state', () => {
+    const initialState = getDefaultState([dog, cat, hamster]);
+
+    expect(imageGalleryReducer(initialState)).toEqual({
+      images: [dog, cat, hamster],
+    });
+  });
+
+  it('Should load the next image', () => {
+    const finalState = [nextImage()].reduce(imageGalleryReducer, defaultInitialState);
+
+    expect(finalState).toEqual({
+      images: [cat, hamster, panda, tiger, dog],
+    });
+  });
+
+  it('Should load next images', () => {
+    const finalState = [nextImage(), nextImage(), nextImage()].reduce(
+      imageGalleryReducer,
+      defaultInitialState
+    );
+
+    expect(finalState).toEqual({
+      images: [panda, tiger, dog, cat, hamster],
+    });
+  });
+
+  it('Should reset to 1st image when getting to the end of pictures', () => {
+    const finalState = [nextImage(), nextImage(), nextImage(), nextImage(), nextImage()].reduce(
+      imageGalleryReducer,
+      defaultInitialState
+    );
+
+    expect(finalState).toEqual(defaultInitialState);
+  });
+
+  it('Should load the previous image', () => {
+    const finalState = [previousImage()].reduce(imageGalleryReducer, defaultInitialState);
+
+    expect(finalState).toEqual({
+      images: [tiger, dog, cat, hamster, panda],
+    });
+  });
+
+  it('Should load previous images', () => {
+    const finalState = [previousImage(), previousImage(), previousImage()].reduce(
+      imageGalleryReducer,
+      defaultInitialState
+    );
+
+    expect(finalState).toEqual({
+      images: [hamster, panda, tiger, dog, cat],
+    });
+  });
+
+  it('Should reset to 1st image when getting at the end of pictures (previous)', () => {
+    const finalState = [
+      previousImage(),
+      previousImage(),
+      previousImage(),
+      previousImage(),
+      previousImage(),
+    ].reduce(imageGalleryReducer, defaultInitialState);
+
+    expect(finalState).toEqual(defaultInitialState);
+  });
+
+  it('Should load next/previous images', () => {
+    const finalState = [nextImage(), previousImage(), previousImage(), nextImage()].reduce(
+      imageGalleryReducer,
+      defaultInitialState
+    );
+
+    expect(finalState).toEqual(defaultInitialState);
+  });
+});
---
+++ pages/components/ImageGallery/reducer.js
@@ -0,0 +1,37 @@
+export const NEXT_IMAGE = 'imageGallery/next';
+export const PREVIOUS_IMAGE = 'imageGallery/previous';
+
+export const nextImage = () => ({
+  type: NEXT_IMAGE,
+});
+
+export const previousImage = () => ({
+  type: PREVIOUS_IMAGE,
+});
+
+export const getDefaultState = images => ({
+  images,
+});
+
+export function imageGalleryReducer(state = getDefaultState([]), action = {}) {
+  switch (action.type) {
+    case NEXT_IMAGE:
+      return {
+        images: calculateNextImages(state.images),
+      };
+    case PREVIOUS_IMAGE:
+      return {
+        images: calculatePreviousImages(state.images),
+      };
+    default:
+      return state;
+  }
+}
+
+function calculateNextImages(images) {
+  return [...images.slice(1), images[0]];
+}
+
+function calculatePreviousImages(images) {
+  return [images[images.length - 1], ...images.slice(0, images.length - 1)];
+}
